203b645820f8d1ce4d99e8d834b2936736897a06
kasia <kat-koz@hotmail.co.uk>
2020-03-10 21:33:18 +0000

    tidy up cli

--- autoblinds/cli.py
+++ autoblinds/cli.py
@@ -4,31 +4,53 @@ import os
 from autoblinds.servos.ServosController import ServosController
 
 
-CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+@click.group()
+def cli():
+    pass
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-a', '--auto',
-              type=click.Choice(['start', 'stop'], case_sensitive=False),
-              help='Starts/Stops automation of the blinds')
-@click.option('-c', '--config',
-              required=False, type=str,
+@cli.group()
+def auto():
+    pass
+
+
+@auto.command('on')
+@click.option('-c', '--config', required=False, type=str,
               default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
               help='Points to the config file defining servos')
-def cli(auto, config):
+def auto_on(config):
     """
-    Starts or stops automated blinds
-    :param auto:
-    :return:
+    Starts automated blinds
     """
-    if auto == 'start':
-        # takes care of the sunrise/sunset process
-        servos_controller = ServosController(config)
-        servos_controller.update_auto(True)
-    elif auto == 'stop':
-        # will end the auto state
-        servos_controller = ServosController(config)
-        servos_controller.update_auto(False)
+    servos_controller = ServosController(config)
+    servos_controller.update_auto(True)
+    servos_controller.engage_servos()
+
+
+@auto.command('off')
+@click.option('-c', '--config', required=False, type=str,
+              default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
+              help='Points to the config file defining servos')
+def auto_off(config):
+    """
+    Stops automated blinds
+    """
+    servos_controller = ServosController(config)
+    servos_controller.update_auto(False)
+
+
+@cli.group()
+def override():
+    pass
+
+
+@override.command()
+@click.option('-c', '--config', required=False, type=str,
+              default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
+              help='Points to the config file defining servos')
+def override_auto():
+    pass
+
 
 if __name__ == '__main__':
     cli()
