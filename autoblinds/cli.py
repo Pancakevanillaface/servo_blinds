77f060f3b73845cb28f92ca4b48a65ab41ec56f1
kasia <kat-koz@hotmail.co.uk>
2020-03-15 20:02:46 +0000

    change up cli, add stopping command

--- autoblinds/cli.py
+++ autoblinds/cli.py
@@ -1,5 +1,6 @@
 import click
 import os
+import yaml
 
 from autoblinds.servos.ServosController import ServosController
 
@@ -9,12 +10,7 @@ def cli():
     pass
 
 
-@cli.group()
-def auto():
-    pass
-
-
-@auto.command('on')
+@cli.command('on')
 @click.option('-c', '--config', required=False, type=str,
               default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
               help='Points to the config file defining servos')
@@ -28,7 +24,7 @@ def auto_on(config):
     servos_controller.schedule_servo_cronjobs()
 
 
-@auto.command('off')
+@cli.command('off')
 @click.option('-c', '--config', required=False, type=str,
               default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
               help='Points to the config file defining servos')
@@ -41,38 +37,48 @@ def auto_off(config):
     servos_controller.write_current_config()
 
 
-@cli.group()
-def override():
-    pass
-
-
-@override.command()
+@cli.command('override')
 @click.option('-c', '--config', required=False, type=str,
               default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
               help='Points to the config file defining servos')
-def override_auto():
+def override(config):
     """
     Changes current state
     """
     pass
 
 
-@cli.group()
-def calibrate():
-    pass
-
-
-@calibrate.command()
+@cli.command('calibrate')
 @click.option('-c', '--config', required=False, type=str,
-                  default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
-                  help='Calibrates servos')
-def calibrate_servo(config):
+              default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
+              help='Calibrates servos')
+def calibrate(config):
     """
     Calibrates servos
     """
     servos_controller = ServosController(config)
     servos_controller.calibrate()
     servos_controller.write_current_config()
+
+
+@cli.command('stop')
+@click.option('-c', '--config', required=False, type=str,
+              default=os.path.join(os.path.dirname(__file__), 'servos_config.yml'),
+              help='Attempts to stop all servos, works if already calibrated')
+def stop(config):
+    """
+    Attempts to stop all servos, works if already calibrated
+    """
+    from adafruit_servokit import ServoKit
+    with open(config) as c:
+        config = yaml.load(c, Loader=yaml.FullLoader)
+    kit = ServoKit(channels=config['ALL_CHANNELS'])
+    for key, val in config.items():
+        if isinstance(key, int):
+            try:
+                kit.servo[key].angle = val['SERVO_DETAILS']['stationary_degrees']
+            except KeyError:
+                pass
 
 
 if __name__ == '__main__':
