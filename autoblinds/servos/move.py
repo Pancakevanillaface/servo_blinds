a7078cd82b08dc102c43bf28f893bc402cc1e327
Kasia Kozlowska <kat-koz@hotmail.co.uk>
2020-03-22 23:28:16 +0000

    add type int to move script

--- autoblinds/servos/move.py
+++ autoblinds/servos/move.py
@@ -39,7 +39,8 @@ if __name__ == '__main__':
     arg_parser.add_argument('-ch',
                             '--channel',
                             help='Servo Channel',
-                            required=True)
+                            required=True,
+                            type=int)
     arg_parser.add_argument('-m',
                             '--movement',
                             help='Whether to perform open or close movement',
